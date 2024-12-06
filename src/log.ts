b4fa0dbe202245a6d7dc6cc85a6dcf38db103459
PS Parwez <infoparwez200@gmail.com>
2024-12-06 06:39:04 +0530

    fix: bugs

--- src/log.ts
+++ src/log.ts
@@ -5,27 +5,32 @@ const errorColor: string = "#ff2323";
 
 //  log function to apply styling
 function logMessage(message: string, color: string, fontWeight: string = "") {
-    console.log(`%c ${message}`, `color: ${color}; ${fontWeight}`);
+  console.log(`%c ${message}`, `color: ${color}; ${fontWeight}`);
 }
 
 // Create a custom console object
-export const customConsole = {
-    success(message: string) {
-        logMessage(message, successColor);
-    },
-    warning(message: string) {
-        logMessage(message, warningColor);
-    },
-    info(message: string) {
-        logMessage(message, infoColor);
-    },
-    error(message: string) {
-        logMessage(message, errorColor, "font-weight: bold");
-    },
+const customConsole = {
+  success(message: string) {
+    logMessage(message, successColor);
+  },
+  warning(message: string) {
+    logMessage(message, warningColor);
+  },
+  info(message: string) {
+    logMessage(message, infoColor);
+  },
+  error(message: string) {
+    logMessage(message, errorColor, "font-weight: bold");
+  },
 };
 
-// Override the global console object
+
+const originalConsole  = globalThis.console;
+
 globalThis.console = {
-    ...console,  // Preserve original console methods
+    ...originalConsole ,  // Keep original console methods
     ...customConsole,  // Add our custom methods
-};
+  };
+
+// Export the global console object
+export const console = globalThis.console;
